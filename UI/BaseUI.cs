15156192dc3dd8fe48ad9dbd993e841c5a5225fc
FazerGamer <48662882+FazerGamer@users.noreply.github.com>
2020-08-17 15:21:22 -0500

    MMMMM

---
+++ UI/BaseUI.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.UI;
+using static Terraria.ModLoader.ModContent;
+using Nocturn.Items.Guns;
+using Nocturn.NPCs;
+
+namespace Nocturn.UI
+{
+
+    internal class BaseUI : UIState
+    {
+        public DragableUIPanel BG;
+        public SmithBG SBG;
+       // public static bool Visible;
+        public override void OnInitialize()
+        {
+            // Here we define our container UIElement. In DragableUIPanel.cs, you can see that DragableUIPanel is a UIPanel with a couple added features
+
+            BG = new DragableUIPanel();
+            BG.SetPadding(0);
+            BG.Left.Set(10f, 0f);
+            BG.Top.Set(400f, 0f);
+            BG.Width.Set(490f, 0f);
+            BG.Height.Set(302f, 0f);
+
+            SBG = new SmithBG(GetTexture("Nocturn/UI/SmithUI"));
+            SBG.Left.Set(0, 0f);
+            SBG.Top.Set(0, 0f);
+            SBG.Width.Set(490, 0f);
+            SBG.Height.Set(302, 0f);
+
+            BG.Append(SBG);
+            Append(BG);
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            // This prevents drawing unless we are using an ExampleDamageItem
+            if (Main.LocalPlayer.talkNPC == -1 || Main.npc[Main.LocalPlayer.talkNPC].type != NPCType<SkeletonSmith>())
+            {
+                GetInstance<Nocturn>().Smith.SetState(null);
+                var modPlayer = Main.LocalPlayer.GetModPlayer<NocturnPlayer>();
+                return;
+            }
+                base.Draw(spriteBatch);
+            
+            
+        }
+        protected override void DrawSelf(SpriteBatch spriteBatch)
+        {
+            base.DrawSelf(spriteBatch);
+            Main.HidePlayerCraftingMenu = true;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (Main.LocalPlayer.talkNPC == -1 || Main.npc[Main.LocalPlayer.talkNPC].type != NPCType<SkeletonSmith>())
+            {
+                GetInstance<Nocturn>().Smith.SetState(null);
+                var modPlayer = Main.LocalPlayer.GetModPlayer<NocturnPlayer>();
+                base.Update(gameTime);
+            }
+        }
+    }
+}
---
+++ UI/SmithBG.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+using Terraria.UI;
+using Microsoft.Xna.Framework.Graphics;
+using static Terraria.ModLoader.ModContent;
+using Terraria.ModLoader;
+
+
+
+namespace Nocturn.UI
+{
+    // This DragableUIPanel class inherits from UIPanel. 
+    // Inheriting is a great tool for UI design. By inheriting, we get the background drawing for free from UIPanel
+    // We've added some code to allow the panel to be dragged around. 
+    // We've also added some code to ensure that the panel will bounce back into bounds if it is dragged outside or the screen resizes.
+    // UIPanel does not prevent the player from using items when the mouse is clicked, so we've added that as well.
+    internal class SmithBG : UIImage
+    {
+
+        public SmithBG(Texture2D texture) : base(texture)
+        {
+            //texture = ModContent.GetTexture("Nocturn/UI/SmithUI");
+            return;
+        }
+        protected override void DrawSelf(SpriteBatch spriteBatch)
+        {
+            base.DrawSelf(spriteBatch);
+
+        }
+    }
+}
